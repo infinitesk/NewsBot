1034eaf1457c9030b3715e56843bd1f4490ef071
Sanidhya <95526618+infinitesk@users.noreply.github.com>
2023-12-26 11:23:01 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,68 @@
+import os
+import streamlit as st
+import pickle
+import time
+from langchain.llms import OpenAI
+from langchain.chains import RetrievalQAWithSourcesChain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import UnstructuredURLLoader
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+
+from dotenv import load_dotenv
+load_dotenv()  # Load environment variables from .env
+
+st.title("News Research Tool 📈")
+st.sidebar.title("News Article URLs")
+
+urls = [st.sidebar.text_input(f"URL {i+1}") for i in range(3)]
+
+process_url_clicked = st.sidebar.button("Process URLs")
+file_path = f"faiss_store_openai_{int(time.time())}.pkl"  # Use a unique filename
+
+main_placeholder = st.empty()
+llm = OpenAI(temperature=0.9, max_tokens=500)
+
+if process_url_clicked:
+    try:
+        # Load data
+        loader = UnstructuredURLLoader(urls=urls)
+        main_placeholder.text("Data Loading...✅")
+        data = loader.load()
+
+        # Split data
+        text_splitter = RecursiveCharacterTextSplitter(
+            separators=['\n\n', '\n', '.', ','],
+            chunk_size=1000
+        )
+        main_placeholder.text("Text Splitter...✅")
+        docs = text_splitter.split_documents(data)
+
+        # Create embeddings and save to FAISS index
+        embeddings = OpenAIEmbeddings()
+        vectorstore_openai = FAISS.from_documents(docs, embeddings)
+        main_placeholder.text("Embedding Vector Building...✅")
+
+        # Save FAISS index to a pickle file
+        with open(file_path, "wb") as f:
+            pickle.dump(vectorstore_openai, f)
+
+    except Exception as e:
+        st.error(f"Error processing URLs: {str(e)}")
+
+query = main_placeholder.text_input("Question: ")
+if query:
+    if os.path.exists(file_path):
+        with open(file_path, "rb") as f:
+            vectorstore = pickle.load(f)
+            chain = RetrievalQAWithSourcesChain.from_llm(llm=llm, retriever=vectorstore.as_retriever())
+            result = chain({"question": query}, return_only_outputs=True)
+
+            # Display answer and sources
+            st.header("Answer")
+            st.write(result["answer"])
+
+            sources = result.get("sources", "")
+            if sources:
+                st.subheader("Sources:")
+                st.write(sources.split("\n"))  # Display sources as a list
